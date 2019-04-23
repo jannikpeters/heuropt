8bd54e6f4885aee508ceef5c77ec9abf42612234
Jannik <jannik@LAPTOP-EKN8JIMS.localdomain>
2019-04-23 15:34:35 +0200

    added 1+1

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,5 @@
 from random import *
+import numpy as np
 def oneMax(bitstring):
     return sum(bitstring)
 def leadingOnes(bitstring):
@@ -10,17 +11,27 @@ def binVal(bitstring):
 def royalRoads(k, bitstring):
     return 0
 def compare(func, first, second):
-    return func(first) > func(second)
+    return func(first) <= func(second)
 
 def opoea(func, n):
+    count = 0
     x = [randint(0,1) for _ in range(n)]
     val = func(x)
-    while(x < n):
-        y = x
-        for(i in range(n)):
-            if(random.random() <= 1/n):
-                y[i] = 1- y[i]
+    while val < n :
+        count+=1
+        y = x.copy()
+        changes = np.random.binomial(n = n, p=(1/n))
+        changeVals = np.random.choice(n, changes)
+        for i in changeVals:
+            y[i] = 1- y[i]
+        if compare(func, x, y):
+            x = y.copy()
+
+        val = func(x)
+    return count
 
 
-
-print(opoea(oneMax, 4))
+n = 25
+while True:
+    print(n, opoea(oneMax, n))
+    n+= 25
