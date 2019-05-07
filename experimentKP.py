f9af6ec7a42780e951f064a2b8a785e5c0c55abb
Anton von Weltzien <a.weltzien@outlook.de>
2019-05-07 15:44:12 +0200

    Adapt for gecco

--- experimentKP.py
+++ experimentKP.py
@@ -18,12 +18,12 @@ def run():
     if os.cpu_count() < 5:
         print('ATTENTION you have a slow pc, thus the problem size has been reduced')
         performance_factor = 1
-    with Pool() as p:
-        p.map(run_for_file, zip(iglob('data/**.ttp'), repeat(performance_factor)))
+    # with Pool() as p:
+    #     p.map(run_for_file, zip(iglob('data/**.ttp'), repeat(performance_factor)))
     # with ProcessPoolExecutor() as executor:
     #     executor.map(run_for_file, zip(iglob('data/**.ttp'), repeat(performance_factor)))
-    # for x in zip(iglob('data/**.ttp'), repeat(performance_factor)):
-    #     run_for_file(x)
+    for x in zip(iglob('geccoinstances/**.ttp'), repeat(performance_factor)):
+        run_for_file(x)
 
 
 def return_bin_vals(n, p):
@@ -34,49 +34,50 @@ def return_bin_vals(n, p):
 def run_for_file(file_performance_factor):
     file, performance_factor = file_performance_factor
     timeout_min = 1 * performance_factor
-    max_knapsack_capacity = 1_000_000
 
     df = pd.DataFrame(columns=['filename', 'algorithm', 'iterations', 'solution', 'time',
                                'kp_capacity', 'item_number', 'optimal_solution', 'aborted',
                                'result_over_time'])
     ttsp = TTSP(file)
-    if ttsp.knapsack_capacity < max_knapsack_capacity:
-        print('Running for file ', file)
 
+    print('Running for file ', file)
+    try:
         dp_res, bin_str, steps, is_aborted, run_time, intermed_vals = DPNumpy(ttsp,
                                                                               timeout_min).optimize()
+    except MemoryError as m:
+        print(m, 'for file', file)
+        is_aborted = True
+        dp_res, bin_str, steps, run_time, intermed_vals = (None,)*5
 
-        optimum = None if is_aborted else dp_res  # must be None so equality is never true
-        df = append_row(df, optimum, ttsp, 'DP_numpy', file, dp_res, bin_str, steps,
-                        is_aborted,
-                        run_time, intermed_vals)
+    optimum = None if  is_aborted else dp_res  # must be None so equality is never true
+    df = append_row(df, optimum, ttsp, 'DP_numpy', file, dp_res, bin_str, steps,
+                    is_aborted,
+                    run_time, intermed_vals)
 
-        value, greedy_bin_str, steps, is_aborted, run_time, intermed_vals = Greedy(
-            ttsp).optimize()
-        df = append_row(df, optimum, ttsp, 'Greedy', file, value, greedy_bin_str, steps,
+    value, greedy_bin_str, steps, is_aborted, run_time, intermed_vals = Greedy(
+        ttsp).optimize()
+    df = append_row(df, optimum, ttsp, 'Greedy', file, value, greedy_bin_str, steps,
+                    is_aborted, run_time, intermed_vals)
+
+    test_case = TestCase(optimum, timeout_min, ttsp)
+
+    algorithms = []
+    # for p in [2, 6]:
+    #     algorithms.append(
+    #         OnePlusOneEA(ttsp, test_case.copy(), np.zeros(ttsp.item_num, dtype=int),
+    #                      'zero_init_bin_p_' + str(p), lambda n: return_bin_vals(n, p / n)))
+    #     algorithms.append(
+    #         OnePlusOneEA(ttsp, test_case.copy(), greedy_bin_str, 'greedy_init_bin_p' +
+    #                      str(p),
+    #                      lambda n: return_bin_vals(n, p / n)))
+    for algo in algorithms:
+        print(algo.name)
+        value, bin_str, steps, is_aborted, run_time, intermed_vals = algo.optimize()
+        df = append_row(df, optimum, ttsp, algo.name, file, value, bin_str, steps,
                         is_aborted, run_time, intermed_vals)
 
-        test_case = TestCase(optimum, timeout_min, ttsp)
-
-        algorithms = []
-        for p in [2, 6]:
-            algorithms.append(
-                OnePlusOneEA(ttsp, test_case.copy(), np.zeros(ttsp.item_num, dtype=int),
-                             'zero_init_bin_p_' + str(p), lambda n: return_bin_vals(n, p / n)))
-            algorithms.append(
-                OnePlusOneEA(ttsp, test_case.copy(), greedy_bin_str, 'greedy_init_bin_p' +
-                             str(p),
-                             lambda n: return_bin_vals(n, p / n)))
-        for algo in algorithms:
-            print(algo.name)
-            value, bin_str, steps, is_aborted, run_time, intermed_vals = algo.optimize()
-            df = append_row(df, optimum, ttsp, algo.name, file, value, bin_str, steps,
-                            is_aborted, run_time, intermed_vals)
-
-        print('Writing result ', file)
-        df.to_csv('results_2/' + file[5:] + '.csv')
-    else:
-        print('Skipped file ', file)
+    print('Writing result ', file)
+    df.to_csv('gecco_solutions/' + file[len('geccoinstances/'):] + '.csv')
 
 
 def append_row(df, optimum, ttsp, algo_name, file, value, assignment, steps, is_timed_out,
