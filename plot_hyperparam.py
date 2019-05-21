2d77b71605556bcb5dc130bfd926786a88077207
Anton von Weltzien <a.weltzien@outlook.de>
2019-05-21 10:00:00 +0200

    Read in results as df

--- plot_hyperparam.py
+++ plot_hyperparam.py
@@ -1,22 +1,27 @@
 from glob import iglob
-
+import pandas as pd
 
 def load_file_names():
     names = ['a280_n279', 'a280_n2790','a280_n1395',
       'fnl4461_n4460', 'fnl4461_n22300', 'fnl4461_n44600',
      'pla33810_n33809', 'pla33810_n169045', 'pla33810_n338090']
 
-    dict = {}
+    rows = []
     for problem_name in names:
-        dict[problem_name] = []
         for file_path in iglob('gecco_solutions/'+problem_name+'/*'):
             file = file_path.split('/')[-1]
             parts = file.split('_')
-            type = parts[2]
+            algo = parts[2]
             profit = int(parts[3][1:])
             c = float(parts[4][1:])
-            print('hi')
+            rows.append({'problem_name': problem_name,
+                   'algo': algo,
+                   'profit': profit,
+                   'c': c})
+    df = pd.DataFrame(rows)
+    return df
 
 
 if __name__ == '__main__':
-    load_file_names()
+    df = load_file_names()
+    print('i')
