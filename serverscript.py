c5cd183356e9be80fcaf9ebe98665e3d8f28ba69
Jannik <jannik@LAPTOP-EKN8JIMS.localdomain>
2019-06-17 23:08:53 +0200

    this is going to take some time

--- serverscript.py
+++ serverscript.py
@@ -8,9 +8,9 @@ from pygmo import *
 from evaluation_function import profit
 from thief_heuristics import read_init_solution_from, save_result, run_greedy
 from ttsp_heuristics import greedy_ttsp
-tour_min = 2613
-tour_max = 6636
-kp_min = 1375387
+tour_min = 185382
+tour_max = 429339
+kp_min = 645150
 def calculate_for(ttsp, ttsp_permutation, omega, renting_r, num_cities, num_items):
     dominated = True
     count = 0
